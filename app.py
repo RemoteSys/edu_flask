2902cd9b8409717e5ad049d8c45f819d89bd6116
Piotr Kramarczy <pieciak1@poczta.onet.pl>
2024-12-11 12:35:21 +0100

    Added <form> and <input> tags in index.html, allowing you to select a file from disk.    app.py    templates/index.html

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pandas as pd
 
 from src import functions as fn
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    path = 'data/xy1.csv'
-    df = pd.read_csv(path) 
-    table = fn.table_html_view(df)
-    img = fn.create_base64(df)
+    # path = 'data/xy1.csv'
+    path = None
+    table = ''
+    name=''
+    img=''
+
+    if path is not None:
+        df = pd.read_csv(path) 
+        table = fn.table_html_view(df)
+        img = fn.create_base64(df)
 
     return render_template('index.html',
-                           name='first table',
+                           name=name,
                            table=table,
                            img64base=img
                            )
