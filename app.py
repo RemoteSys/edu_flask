cac9ef604357bf88af3327ffeae40bf034065ae2
Piotr Kramarczy <pieciak1@poczta.onet.pl>
2024-12-11 14:04:22 +0100

    Target functionality: - sends files to the server, displays on the screen - the 'chart' button creates a chart, preserving the data table view     app.py     templates/index.html

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import pandas as pd
 
 from src import functions as fn
 
 app = Flask(__name__)
+app.secret_key = "secure key"
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -14,13 +15,23 @@ def index():
     img=''
     
     if  request.method == 'POST':
-        # <input type="file" id="myfile" name="csv_file"><br><br>
-        # use name from input tag to upload file
-        file = request.files['csv_file']
-        name = file.filename
-        df = pd.read_csv(file) 
-        table = fn.table_html_view(df)
-        img = fn.create_base64(df)
+        print(f"\n\n{request.form}\n\n")
+        if 'csv_file' in request.files:
+            # <input type="file" id="myfile" name="csv_file"><br><br>
+            # use name from input tag to upload file
+            file = request.files['csv_file']
+            name = file.filename
+            df = pd.read_csv(file) 
+            table = fn.table_html_view(df)
+            # make df 'global' between sessions
+            session['df'] = df.to_json()
+            session['name'] = name 
+            session['table'] = table
+        elif 'chart' in request.form:
+            df = pd.read_json(session['df'])
+            name = session['name']
+            table = session['table']
+            img = fn.create_base64(df)
 
     return render_template('index.html',
                            name=name,
