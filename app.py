05e11727c317197a089e2568e3110ef5d102732e
Piotr Kramarczy <pieciak1@poczta.onet.pl>
2024-12-18 09:42:30 +0100

    app.py
    - splitting one large index() function into three separate ones (each specialized in one task): data_upload(), create_chart(), index()
    - new route added: /chart /upload
    condition removed: `if request.method == 'POST':`
    
    templates/index.html
    - condition added: {{ if img64base %}
    - new route added: /chart /upload

--- app.py
+++ app.py
@@ -10,30 +10,54 @@ app.secret_key = "secure key"
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # path = 'data/xy1.csv'
     table = ''
     name = ''
     img = ''
 
-    if request.method == 'POST':
-        print(f"\n\n{request.form}\n\n")
-        if 'csv_file' in request.files:
-            # <input type="file" id="myfile" name="csv_file"><br><br>
-            # use name from input tag to upload file
-            file = request.files['csv_file']
-            name = file.filename
-            df = pd.read_csv(file)
-            table = fn.table_html_view(df)
-            # make df 'global' between sessions
-            session['df'] = df.to_json()
-            session['name'] = name
-            session['table'] = table
-        elif 'chart' in request.form:
-            tmp = io.StringIO(session['df'])
-            df = pd.read_json(tmp)
-            name = session['name']
-            table = session['table']
-            img = fn.create_base64(df)
+    return render_template('index.html',
+                           name=name,
+                           table=table,
+                           img64base=img
+                           )
+
+
+@app.route('/upload', methods=['POST'])
+def data_upload():
+    table = ''
+    name = ''
+    img = ''
+
+    if 'csv_file' in request.files:
+        # <input type="file" id="myfile" name="csv_file"><br><br>
+        # use name from input tag to upload file
+        file = request.files['csv_file']
+        name = file.filename
+        df = pd.read_csv(file)
+        table = fn.table_html_view(df)
+        # make df 'global' between sessions
+        session['df'] = df.to_json()
+        session['name'] = name
+        session['table'] = table
+
+    return render_template('index.html',
+                           name=name,
+                           table=table,
+                           img64base=img
+                           )
+
+
+@app.route('/chart', methods=['POST'])
+def create_chart():
+    table = ''
+    name = ''
+    img = ''
+
+    if 'chart' in request.form:
+        tmp = io.StringIO(session['df'])
+        df = pd.read_json(tmp)
+        name = session['name']
+        table = session['table']
+        img = fn.create_base64(df)
 
     return render_template('index.html',
                            name=name,
